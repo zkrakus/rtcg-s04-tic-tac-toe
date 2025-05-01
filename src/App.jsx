4845a466b7599bca25e7b2dc48d833538681a31b
zkrakus <zkrakus@gmail.com>
2025-05-01 14:49:37 -0400

    feat:s05-l082 Added logic to handle selecting squares and started adding logic to show active player.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
+
+import Player from './components/Player'
+import GameBoard from './components/GameBoard';
 
 function App() {
-  
+  const [activePlayer, setActivePlayer] = useState('X')
+
+  function handleSelectSquare() {
+    setActivePlayer((currentlyActivePlayer) => currentlyActivePlayer === 'X' ? 'O' : 'X');
+  }
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare}/>
+      </div>
+      Log
+    </main>
+  );
 }
 
-export default App
+export default App;
